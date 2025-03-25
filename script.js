c6d40262614ee26a6af678debd92dd73cb959a20
MATHEUS BUENO MOREIRA DOS SANTOS <24122472@CFP127ALUNO.LOCAL>
2025-03-25 16:12:52 -0300

    tkk

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("btn-acessar").addEventListener("click", function() {
+        window.location.href = "https://bueninkt.github.io/tela-api/"; // Altere para a URL da nova página hospedada no GitHub
+    });
+});
